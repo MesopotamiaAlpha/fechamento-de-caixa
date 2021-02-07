bceea65beea6d028d0f7e627f57fe425cc8bee37
MesopotamiaAlpha <leao-gomes@hotmail.com>
2021-02-07 15:21:13 -0300

    Adicionando o firesbase

---
+++ js/fire.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDps-F6PE4pbPobCrTivA-VbBC_RCy5Di0",
+    authDomain: "fechamento-caixa.firebaseapp.com",
+    projectId: "fechamento-caixa",
+    storageBucket: "fechamento-caixa.appspot.com",
+    messagingSenderId: "107237184089",
+    appId: "1:107237184089:web:2043c2bc7c55f6fe1dd748",
+    measurementId: "G-ZPV9V2M15V"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
