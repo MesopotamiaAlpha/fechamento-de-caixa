e581a47f14fd8e5b601046f80b939dd9dcb15fb6
MesopotamiaAlpha <leao-gomes@hotmail.com>
2022-12-19 23:15:09 -0300

    Criado barra superior, ainda é necessario terminar

---
+++ src/components/barraCima/BarraCima.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './BarraCima.css';
+
+const BarraCima = () => {
+    return  (
+        <div className="barraCima">
+            <div className="barraMenor">
+                Este texto está na barra menor
+            </div>
+
+            Componente da barra de cima
+        </div>
+
+        
+    )
+}
+
+
+export default BarraCima;
