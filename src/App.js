8705af74ea03ba08e124afef0b58aada8fabe920
MesopotamiaAlpha <leao-gomes@hotmail.com>
2022-12-20 21:16:47 -0300

    Criando barra lateral

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './css/App.css';
 import './components/blocoValores/valores'
 import Blocovalores from './components/blocoValores/valores';
 import BarraCima from './components/barraCima/BarraCima';
+import BarraLateral from './components/barraLateral/barraLateral';
 
 function App(props) {
   return (
     <div className="App">
       <Blocovalores className="blocoValores" />
       <BarraCima className="barraCima" />
+      <BarraLateral className="barraLateral" />
 
     </div>
   );
---
+++ src/components/barraLateral/barraLateral.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BarraLateral = () => {
+
+    return (
+        <div className="barra">
+            <h1> Texto dentro da lateral </h1>
+        </div>
+    )
+
+}
+
+export default BarraLateral;
